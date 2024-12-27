02fff6d80b0aac1a91bc0a2ca3393d3b1e7c10e1
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2024-12-27 03:37:28 -0800

    Images

---
+++ src/component/Carousel/imgs/data.js
@@ -0,0 +1,11 @@
+import img1 from "./Africa.jpg";
+import img2 from "./HAbesha.jpg";
+import img3 from "./Hbook.jpg";
+import img4 from "./Hbag.jpg";
+import img5 from "./Hbagdis.jpg";
+import img6 from "./Hfood.jpg";
+import img7 from "./Hhotel.jpg";
+
+
+
+export const img = [img1,img2, img3,img4,img5,img6,img7 ];
