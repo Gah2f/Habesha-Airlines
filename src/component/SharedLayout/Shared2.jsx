6af1b446d508df318d5845cda9f59ec4f57e1229
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2024-12-31 23:01:35 -0800

    Display

---
+++ src/component/SharedLayout/Shared2.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../Header/Header'
+import Payment from '../Payment/Payment'
+import Footer from '../Footer/Footer'
+function Shared2() {
+  return (
+    <div>
+        <Header/>
+        <Payment/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Shared2
