c4b6a383fc7f7793dbf69ec6907fd26b47ebce65
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2024-12-31 23:03:13 -0800

    Display

---
+++ src/component/SharedLayout/Shared5.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../Header/Header'
+import Book from '../Book/Book'
+import Footer from '../Footer/Footer'
+
+function Shared5() {
+  return (
+    <div>
+        <Header/>
+        <Book/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Shared5
