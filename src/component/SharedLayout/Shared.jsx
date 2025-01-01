ba7757c82dfe1ec74738fcb12c51005e4378664f
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2024-12-31 23:00:57 -0800

    Display

---
+++ src/component/SharedLayout/Shared.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../Header/Header'
+import Banner from '../Banner/Banner'
+import Footer from '../Footer/Footer'
+import CaroEfffect from '../Carousel/CaroEfffect'
+
+function Shared() {
+return (
+    <div>
+        <Header/>
+        <CaroEfffect/>
+        <Banner/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Shared
