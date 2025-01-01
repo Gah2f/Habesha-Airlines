dfb7db2f2c198b7d3689ad0319d155e1e481da63
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2024-12-31 23:02:37 -0800

    Display

---
+++ src/component/SharedLayout/Shared4.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Readytopay from '../Readytopay/Readytopay'
+
+function Shared4() {
+  return (
+    <div>
+        <Readytopay/>
+    </div>
+  )
+}
+
+export default Shared4
